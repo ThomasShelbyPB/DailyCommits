2f716df2079176f2c3f885e4b4bf0a3d56463dfe
ThomasShelbyPB <38157799+ThomasShelbyPB@users.noreply.github.com>
2024-06-19 00:53:55 +0530

    Add files via upload

---
+++ tensor.py
@@ -0,0 +1,5 @@
+"""
+A tensor is just a n dimensionl array
+"""
+
+from numpy import ndarray as Tensor
---
+++ xor.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+
+from atul.train import train
+from atul.nn import NeuralNet
+from atul.layers import Linear, Tanh
+
+inputs = np.array([
+    [0, 0],
+    [1, 0],
+    [0, 1],
+    [1, 1]
+])
+
+targets = np.array([
+    [1, 0],
+    [0, 1],
+    [0, 1],
+    [1, 0]
+])
+
+net = NeuralNet([
+    Linear(input_size=2, output_size=2),
+    Tanh(),
+    Linear(input_size=2, output_size=2)
+])
+
+train(net, inputs, targets)
+
+for x, y in zip(inputs, targets):
+    predicted = net.forward(x)
+    print(x, predicted, y)
